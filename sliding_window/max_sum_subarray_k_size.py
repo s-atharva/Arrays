02f016560fcf0e3252f18cb187d092fec51f5b3e
s-atharva <atharvasarbhukan@gmail.com>
2025-09-08 17:11:27 +0530

    Add max sum subarray of size k problem

--- sliding_window/max_sum_subarray_k_size.py
+++ sliding_window/max_sum_subarray_k_size.py
@@ -1,28 +1,24 @@
 from typing import List
 
 
-def find_max_sum_subarray(my_list: List[int], k: int) -> int:
-    length = len(my_list)
-    i = 0
-    j = 0
-    total = 0
-    max_sum = float('-inf')
-
-    while j < length:
-        total += my_list[j]
-
-        if j - i + 1 < k:
-            j += 1
-        elif j - i + 1 == k:
-            if total > max_sum:
-                max_sum = total
-            total -= my_list[i]
-            i += 1
-            j += 1
-
-    return max_sum
+def find_max_sum_subarray(arr: List[int], window_size: int) -> List[int]:
+    start = 0
+    end = 0
+    n = len(arr)
+    output = []
+    sum = 0
+    while end < n:
+        sum += arr[end]
+        if end - start + 1 < window_size:
+            end += 1
+        else:
+            output.append(sum)
+            sum -= arr[start]
+            start += 1
+            end += 1
+    return output
 
 
-my_list = [2, 3, 5, 2, 9, 7, 1]
+my_list: List[int] = [1, 2, 3, 4, 5, 6]
 k = 3
-print(find_max_sum_subarray(my_list=my_list, k=k))
+print(find_max_sum_subarray(arr=my_list, window_size=k))
